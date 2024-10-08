812dd33e924babbddcc70598395410d0de883140
ASMAXI <108028037+ASMAXI@users.noreply.github.com>
2024-10-08 22:25:26 +0300

    Update RoleService.java

--- src/main/java/ru/kata/spring/boot_security/demo/service/RoleService.java
+++ src/main/java/ru/kata/spring/boot_security/demo/service/RoleService.java
@@ -5,7 +5,7 @@ import ru.kata.spring.boot_security.demo.model.Role;
 import java.util.List;
 
 public interface RoleService {
-    public List<Role> getAllRoles();
-    public Role getRoleById(Long id);
-    public Role findRoleByName(String name);
+     List<Role> getAllRoles();
+     Role getRoleById(Long id);
+     Role findRoleByName(String name);
 }
